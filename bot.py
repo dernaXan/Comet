7fae8250eb426f278cfaf0e05e5c390fc88af93d
Tarani-2020 <97033898+Tarani-2020@users.noreply.github.com>
2025-07-06 01:45:35 +0200

    bot.py aktualisieren

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import os
+import aiohttp
+import json
 
 intents = discord.Intents.default()
 
@@ -11,7 +13,34 @@ async def on_ready():
     print(f"{bot.user} ist online!", flush=True)
 
 #intern functions
+async def get_data(server_id, user):
+    url = f"{os.getenv('DB_API_URL')}/data/get/{server_id}/user/{user}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            data = await resp.json()
+    return data
+    
+async def save_data(server_id, user, data):
+    url = f"{os.getenv('DB_API_URL')}/data/save/{server_id}/user/{user}"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data) as resp:
+            ans = await resp.text()
+    return ans
+    
+#slash commands
+@bot.slash_command(name="addpoints")
+@commands.has_permission(administrator=True)
+async def addpoints(ctx, member:discord.Member, points:int)
+    data = await get_data(ctx.guild.id, member.id)
+    data["points"] += points
+    await save_data(ctx.guild.id, member.id, data)
 
+@bot.slash_command(name="subtractpoints")
+@commands.has_permission(administrator=True)
+async def subtractpoints(ctx, member:discord.Member, points:int)
+    data = await get_data(ctx.guild.id, member.id)
+    data["points"] -= points
+    await save_data(ctx.guild.id, member.id, data)
 
 def start():
     TOKEN = os.getenv("DISCORD_TOKEN")
