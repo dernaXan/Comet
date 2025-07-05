60f0ff757c8411c669dd538a050864b6ebc55276
Tarani-2020 <97033898+Tarani-2020@users.noreply.github.com>
2025-07-05 17:33:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,16 @@
+import os
 import discord
 from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+  print(f"Bot ist eingeloggt als {bot.user}")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+if TOKEN is None:
+  print("ERROR: Kein Token!")
+  exit()
+bot.run(TOKEN)
