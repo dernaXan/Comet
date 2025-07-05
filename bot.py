edc2937204945c34d44a6e93b1e79e752e3df713
Tarani-2020 <97033898+Tarani-2020@users.noreply.github.com>
2025-07-05 19:07:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,5 +9,5 @@ async def on_ready():
     print(f"{bot.user} ist online!")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-print("RUNNING BOT...", flush=True)
+print(f"RUNNING BOT...\nRunning with Token: {TOKEN}", flush=True)
 bot.run(TOKEN)
