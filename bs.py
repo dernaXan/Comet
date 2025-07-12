6c56db1100ae5d63ace6cf09d971727166f629dc
Tarani-2020 <97033898+Tarani-2020@users.noreply.github.com>
2025-07-12 22:53:33 +0200

    Create bs.py

---
+++ bs.py
@@ -0,0 +1,19 @@
+import requests
+import os
+
+API_TOKEN = os.environ.get("BS_API_KEY")
+BASE_URL = "https://api.brawlstars.com/v1"
+
+headers = {
+  "Authorization": f"Bearer {API_KEY}"
+}
+
+def get_player(player_tag):
+  url = f"{BASE_URL}/players{player_tag.replace('#', '%23')}"
+  r = requests.get(url, headers=headers)
+
+  if r.status_code == 200:
+    data = r.json()
+    return data
+  else:
+    return r.json()
